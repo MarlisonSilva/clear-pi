2cdad1de713dac8d128df9ae2e927829690b7536
Marlison S. S <marlison.silva@escolar.ifrn.edu.br>
2021-08-27 14:53:00 -0300

    bootstrap com theming

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -8,20 +8,45 @@
     <link rel="stylesheet" href="{{ asset('assets/css/map.css') }}">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.4.1/mapbox-gl.js"></script>
     
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
     <title>Mapa</title>
 </head>
 <body>
     <div class="side-bar">
-        <ul>
-            <li>#213242</li>
-            <li>#3c5a72</li>
-            <li>#517490</li>
-            <li>#e3a92b</li>
-            <li>#cd9a03</li>
-            <li>#e1c888</li>
+        {{-- <ul class="nav nav-tabs">
+            <li class="nav-item">#213242</li>
+            <li class="nav-item">#3c5a72</li>
+            <li class="nav-item">#517490</li>
+            <li class="nav-item">#e3a92b</li>
+            <li class="nav-item">#cd9a03</li>
+            <li class="nav-item">#e1c888</li>
+        </ul> --}}
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
         </ul>
+        
+        <button type="button" class="btn btn-lg btn-test">Teste</button>
+        <button type="button" class="btn btn-lg btn-teste">Teste</button>
     </div>
     <div id="map"></div>
     <script src="{{ asset('assets/js/map.js') }}"></script>
+    
+    <!-- BOOTSTRAP -->
+    <script src="{{ asset('assets/js/jquery.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js')}}"></script>
+
 </body>
 </html>
