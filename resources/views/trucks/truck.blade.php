303a28b0a192e63db2419ac5f4348000587dbb7c
Alec Yalcin <alec.yalcin@escolar.ifrn.edu.br>
2021-09-09 18:33:56 -0300

    Arrumado Exemplos de Edit e Create para as Tabelas Front

--- resources/views/trucks/truck.blade.php
+++ resources/views/trucks/truck.blade.php
@@ -3,7 +3,7 @@
 <h1>Listagem de Caminhões!</h1> 
 <h2>
     <a href="#">
-        <i class="bi bi-plus-circle-fill"></i>
+        <a href="{{ route('trucksTemp.create') }}" ><i class="bi bi-plus-circle-fill"></i></a>
     </a>
 </h2>
 <table>
@@ -14,20 +14,20 @@
         <th>Dia(s) de Funcionamento</th>
     </tr>
     <tr>
-        <td><a href="#indexId">Carlinho Do Teste</a></td>
+        <td><a href="{{ route('trucksTemp.edit', 0) }}"">Carlinho Do Teste</a></td>
         <td>1</td>
         <td>13h~15h</td>
         <td>Quarta/Sábado/Quinta</td>
 
     </tr>
     <tr>
-        <td><a href="#indexId">Patricio Testaria</a></td>
+        <td><a href="{{ route('trucksTemp.edit', 0) }}"">Patricio Testaria</a></td>
         <td>22</td>
         <td>11h~13h</td>
         <td>Segunda/Terça</td>
     </tr>
     <tr>
-        <td><a href="#indexId">Robson Testas</a></td>
+        <td><a href="{{ route('trucksTemp.edit', 0) }}"">Robson Testas</a></td>
         <td>336</td>
         <td>18h~21h</td>
         <td>Sexta</td>
