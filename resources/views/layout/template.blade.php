514d4b37e26eb0398f173a7b3a0ddbbd13f884ab
Marlison S. S <marlisonsoaresjp@gmail.com>
2021-09-09 09:36:39 -0300

    alterações de indexes e template

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -74,7 +74,7 @@
                         <ul class="dropdown-menu">
                           <li><a class="dropdown-item" href="#">Sair</a></li>
                         </ul>
-                    </div>
+                      </div>
                 </div>
             </nav>
 
@@ -94,6 +94,108 @@
             $('#sidebarCollapse').on('click', function () {
                 $('#sidebar').toggleClass('active');
                 $('#content').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gerencia</title>
+
+    {{-- BOOSTRAP STYLE --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    {{-- CSS ORIGINAL --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/homepage.css') }}">
+
+</head>
+<body>
+    {{-- Main --}}
+    <div class="row vh-100">
+        <div class="navigation-bar" id="sidebar">
+            {{-- SideBar --}}
+            <nav class="navigation">
+                <div class="text-center">
+                    <h3>C.L.E.A.R</h3>
+                </div>
+                <hr>
+                <ul class="navigation-content">
+                    <li class="navigation-item">
+                        <a href="{{ url('/') }}">
+                            <i id="icon1" class="bi bi-house-fill"></i><span> Página Principal</span>
+                        </a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="{{ url('/zone') }}">
+                            <i id="icon2" class="bi bi-globe"></i><span> Zonas</span>
+                        </a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="{{ url('/truck') }}">
+                            <i id="icon3" class="bi bi-truck"></i><span> Caminhões</span>
+                        </a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="{{ url('/worker') }}">
+                            <i id="icon4" class="bi bi-people-fill"></i><span> Funcionários</span>
+                        </a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="#" onclick="toggleMode();"  id="theme">
+                            <i id="icon5" class="bi bi-moon-stars-fill"></i><span> Mudar Tema</span>
+                        </a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="{{ url('/running') }}">
+                            <i id="icon6" class="i bi-stopwatch-fill"></i><span> Começar coleta</span>
+                        </a>
+                    </li>
+                    <hr>
+                </ul>
+            </nav>
+        </div>
+
+        {{-- Page Content --}}
+        <div class="col container-content">
+
+            <nav class="navbar header">
+                <button type="button" id="sidebarCollapse" class="btn btn-dark nav-item ms-3">
+                    <i class="bi bi-justify"></i>
+                </button>
+                <div id="user" class="dropstart">
+                    <a href="#" class="d-flex" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://clipground.com/images/user-icon-png-free-2.jpg" alt="" width="32" height="32" class="rounded-circle me-2">
+                    </a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Sair</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="container">
+                @yield('content')
+            </div>  
+        </div>
+        
+        
+    </div>
+
+    
+
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/js/darkmode.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('.navigation').toggleClass('disable');
             });
         });
     </script>
--- resources/views/workers/indexWorkerF.blade.php
+++ resources/views/workers/indexWorkerF.blade.php
@@ -1,37 +1,59 @@
 @extends('layout.template')
 @section('content')
-<h1>Funcionários! - FRONT-END</h1>
-<a href="{{ route('workersTemp.create') }}">CRIAR</a>
-<table>
-    <thead>
-        <tr>
-            <th>Ação</th>
-            <th>Bla bla bla</th>
-            <th>Bla bla bla</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>
-                <a href="{{ route('workersTemp.edit', 0) }}">Editar</a>
-            </td>
-            <td>Bla bla bla</td>
-            <td>Bla bla bla</td>
-        </tr>
-        <tr>
-            <td>
-                <a href="{{ route('workersTemp.edit', 1) }}">Editar</a>
-            </td>
-            <td>Bla bla bla</td>
-            <td>Bla bla bla</td>
-        </tr>
-        <tr>
-            <td>
-                <a href="{{ route('workersTemp.edit', 2) }}">Editar</a>
-            </td>
-            <td>Bla bla bla</td>
-            <td>Bla bla bla</td>
-        </tr>
-    </tbody>
-</table>
+<h1 class="mt-5">Funcionários! - FRONT-END</h1>
+<div class="card">
+    <div class="row card-header m-0 w-100 py-2">
+        <div class="card-title col-11 m-0 align-middle">
+            <h5 class="m-0 py-2">
+                Tabela de Funcionários
+
+            </h5>
+        </div>
+        <div class="col-1 align-middle py-2">
+            <a href="{{ route('workersTemp.create') }}" ><i class="bi bi-plus-circle-fill">CRIAR</i></a>
+        </div>
+    </div>
+    
+    <div class="card-body">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Ação</th>
+                    <th scope="col">Bla bla bla</th>
+                    <th scope="col">Bla bla bla</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <a href="{{ route('workersTemp.edit', 0) }}">Editar</a>
+                        <a href="{{ route('workersTemp.edit', 0) }}">Deletar</a>
+                    </td>
+                    <td>Bla bla bla</td>
+                    <td>Bla bla bla</td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="{{ route('workersTemp.edit', 1) }}">Editar</a>
+                        <a href="{{ route('workersTemp.edit', 0) }}">Deletar</a>
+
+                    </td>
+                    <td>Bla bla bla</td>
+                    <td>Bla bla bla</td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="{{ route('workersTemp.edit', 2) }}">Editar</a>
+                        <a href="{{ route('workersTemp.edit', 0) }}">Deletar</a>
+
+                    </td>
+                    <td>Bla bla bla</td>
+                    <td>Bla bla bla</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</div>
+
 @endsection
