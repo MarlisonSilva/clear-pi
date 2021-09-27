26523695c2368416613b891eef3da5b3cc7189e9
Faltrenn <a.emanuel@escolar.ifrn.edu.br>
2021-09-27 00:11:23 -0300

    Código ocioso removido

--- app/Http/Controllers/web/TruckController.php
+++ app/Http/Controllers/web/TruckController.php
@@ -97,17 +97,6 @@ class TruckController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
--- app/Http/Controllers/web/ZoneController.php
+++ app/Http/Controllers/web/ZoneController.php
@@ -72,17 +72,6 @@ class ZoneController extends Controller
     }
     
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-    
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
